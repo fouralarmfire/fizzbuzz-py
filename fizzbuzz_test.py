06308c4698d2d4933765b7708afee038cf7ff529
Claudia Beresford <cberesford@pivotal.io>
2019-05-15 21:07:56 +0100

    part 5

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -9,5 +9,17 @@ class TestFizzBuzz(unittest.TestCase):
     def test_that_number_is_not_divisible_by_three(self):
         self.assertFalse(fizzbuzz.is_divisible_by_three(1))
 
+    def test_that_number_is_divisible_by_five(self):
+        self.assertTrue(fizzbuzz.is_divisible_by_five(5))
+
+    def test_that_number_is_not_divisible_by_five(self):
+        self.assertFalse(fizzbuzz.is_divisible_by_five(1))
+
+    def test_that_number_is_divisible_by_three_and_five(self):
+        self.assertTrue(fizzbuzz.is_divisible_by_three_and_five(15))
+
+    def test_that_number_is_not_divisible_by_three_and_five(self):
+        self.assertFalse(fizzbuzz.is_divisible_by_three_and_five(1))
+
 if __name__ == '__main__':
     unittest.main()
