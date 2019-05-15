35dbc6930e67f085473ec66f995346d07807d771
Claudia Beresford <cberesford@pivotal.io>
2019-05-15 21:13:03 +0100

    part 6

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -4,22 +4,22 @@ import fizzbuzz
 class TestFizzBuzz(unittest.TestCase):
 
     def test_that_number_is_divisible_by_three(self):
-        self.assertTrue(fizzbuzz.is_divisible_by_three(3))
+        self.assertTrue(fizzbuzz.is_divisible_by(3, 3))
 
     def test_that_number_is_not_divisible_by_three(self):
-        self.assertFalse(fizzbuzz.is_divisible_by_three(1))
+        self.assertFalse(fizzbuzz.is_divisible_by(1, 3))
 
     def test_that_number_is_divisible_by_five(self):
-        self.assertTrue(fizzbuzz.is_divisible_by_five(5))
+        self.assertTrue(fizzbuzz.is_divisible_by(5, 5))
 
     def test_that_number_is_not_divisible_by_five(self):
-        self.assertFalse(fizzbuzz.is_divisible_by_five(1))
+        self.assertFalse(fizzbuzz.is_divisible_by(1, 5))
 
     def test_that_number_is_divisible_by_three_and_five(self):
-        self.assertTrue(fizzbuzz.is_divisible_by_three_and_five(15))
+        self.assertTrue(fizzbuzz.is_divisible_by(15, 15))
 
     def test_that_number_is_not_divisible_by_three_and_five(self):
-        self.assertFalse(fizzbuzz.is_divisible_by_three_and_five(1))
+        self.assertFalse(fizzbuzz.is_divisible_by(1, 15))
 
 if __name__ == '__main__':
     unittest.main()
