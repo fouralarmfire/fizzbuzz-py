5a3490bf74ab8a1d06d90aa5f5aba066d5e09b03
Claudia Beresford <cberesford@pivotal.io>
2019-05-15 20:20:54 +0100

    part 2

---
+++ fizzbuzz_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def test_that_number_is_divisible_by_three(self):
+        self.assertTrue(fizzbuzz.is_divisible_by_three(3))
+
+if __name__ == '__main__':
+    unittest.main()
